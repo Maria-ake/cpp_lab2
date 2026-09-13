935db4f46bd2804b216de7787f69d413db070eb2
Maria-ake <mkaustralia@yandex.ru>
2026-09-13 19:38:29 +0700

    Ver2.0

--- tests/test_ip_filter.cpp
+++ tests/test_ip_filter.cpp
@@ -1,6 +1,12 @@
 #include <gtest/gtest.h>
 #include "ip_filter.h"
 
+#include <algorithm>
+#include <iterator>
+#include <tuple>
+#include <vector>
+#include <string>
+
 // Тест 1: Проверка парсинга IP-адреса
 TEST(IpFilterTest, ParseIp) {
     std::string line = "1.2.3.4\tignore\tignore";
@@ -21,7 +27,6 @@ TEST(IpFilterTest, SortOrder) {
     std::sort(pool.begin(), pool.end(), [](const auto& a, const auto& b) {
         return a > b;
     });
-    // Ожидаем: 1.10.1.1, 1.2.1.1, 1.1.1.1
     EXPECT_EQ(std::get<1>(pool[0]), 10);
     EXPECT_EQ(std::get<1>(pool[1]), 2);
     EXPECT_EQ(std::get<1>(pool[2]), 1);
@@ -37,7 +42,7 @@ TEST(IpFilterTest, FilterFirstByte) {
     auto filter = [](const IpAddress& ip) { return std::get<0>(ip) == 1; };
     IpPool result;
     std::copy_if(pool.begin(), pool.end(), std::back_inserter(result), filter);
-    
+
     ASSERT_EQ(result.size(), 1);
     EXPECT_EQ(std::get<0>(result[0]), 1);
 }
