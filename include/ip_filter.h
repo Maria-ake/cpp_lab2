4a7253726f6d4eded2d65b86b318e01d92eb6b8e
Maria-ake <mkaustralia@yandex.ru>
2026-09-14 11:05:47 +0700

    Lab2.0

--- include/ip_filter.h
+++ include/ip_filter.h
@@ -8,6 +8,6 @@
 using IpAddress = std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>;
 using IpPool = std::vector<IpAddress>;
 
-IpPool filter(const IpPool& pool, bool (*condition)(const IpAddress&));
+
 void print_pool(const IpPool& pool);
 IpAddress parse_ip(const std::string& line);
